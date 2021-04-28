6eb67479ffdb15cdf5bfd7287250b992a9decc7a
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-28 15:47:51 -0300

    refactor action
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,12 +6,12 @@ if [[ -z $AWS_CLI_VERSION ]];
 then
     echo "AWS_CLI_VERSION environment variable isn't set."
     echo "Installing AWS CLI latest version"
-    pip3 --no-cache-dir install --upgrade awscli
+    sudo pip3 --no-cache-dir install --upgrade awscli
 else
     echo "Installing AWS CLI version AWS_CLI_VERSION"
-    pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
+    sudo pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
 fi
 
 echo "Executing command: aws $1"
 
-sh -c "aws $1"
+sh -c "sudo aws $1"
