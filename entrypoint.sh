a7cb11e0542ec3e8ce45463b278c5b4d4f9e9dc6
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-28 16:53:13 -0300

    update entrypoint.sh file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,22 +6,12 @@ if [[ -z $AWS_CLI_VERSION ]]
 then
     echo "AWS_CLI_VERSION environment variable isn't set."
     echo "Installing AWS CLI latest version"
-    sudo pip3 --no-cache-dir install --upgrade awscli
+    pip3 --no-cache-dir install --upgrade awscli
 else
     echo "Installing AWS CLI version AWS_CLI_VERSION"
-    sudo pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
-fi
-
-if [[ -z $AWS_ACCESS_KEY_ID ]]
-then
-    mkdir ~/.aws
-    cat >> ~/.aws/config
-    [default]
-    aws_access_key_id=$AWS_ACCESS_KEY_ID
-    aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
-    region=$AWS_DEFAULT_REGION
+    pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
 fi
 
 echo "Executing command: aws $1"
 
-sudo sh -c "aws $1"
+sh -c "aws $1"
