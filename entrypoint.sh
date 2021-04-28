3c6209af4d703f5480a34b6ba271019121acdb42
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-28 16:07:12 -0300

    update entrypoint.sh file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,15 @@ else
     sudo pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
 fi
 
+if [[ -z $AWS_ACCESS_KEY_ID ]]
+then
+    mkdir ~/.aws
+    cat >> ~/.aws/config
+    [default]
+    aws_access_key_id=$AWS_ACCESS_KEY_ID
+    aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
+    region=$AWS_DEFAULT_REGION
+
 echo "Executing command: aws $1"
 
 sudo sh -c "aws $1"
