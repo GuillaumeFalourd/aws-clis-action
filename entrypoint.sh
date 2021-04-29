7e07dc2f66655473633d2b12b41406fcdaaf6e5d
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-29 16:54:16 -0300

    update entrypoint.sh file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,33 +17,24 @@ then
     echo "AWS_CLI_VERSION environment variable isn't set."
     echo "Installing AWS CLI latest version"
     pip3 --no-cache-dir install --upgrade awscli
-    aws --version
+    sh -c "aws --version"
 elif [[ ${AWS_CLI_VERSION:0:2} == "1." ]]
 then
     echo "Installing AWS CLI version AWS_CLI_VERSION"
     pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
-    aws --version
+    sh -c "aws --version"
 elif [[ ${AWS_CLI_VERSION:0:2} == "2." ]]
 then
     echo "Installing AWS CLI version AWS_CLI_VERSION"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64-$AWS_CLI_VERSION.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
-    echo "WHICH AWS"
-    which aws
-    echo "LS"
-    ls -lha
-    echo "CD"
-    cd /usr/local/bin/aws
-    echo "LS"
-    ls -lha
-    echo "AWS VERSION"
-    /usr/local/bin/aws --version
+    sh -c "aws --version"
 else
     echo "AWS_CLI_VERSION environment variable wasn't recognized"
     echo "Installing AWS CLI latest version"
     pip3 --no-cache-dir install --upgrade awscli
-    aws --version
+    sh -c "aws --version"
 fi
 
 echo "Executing command: aws $1"
