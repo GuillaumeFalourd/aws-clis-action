99266f774cd9b65a754ff869aa58ea8598b8f1be
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-30 15:50:42 -0300

    update entrypoint file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-if [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "1." ]]
-then
+if [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "1." ]]; then
+
     echo "Installing AWS CLI version $AWS_CLI_VERSION"
     pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
     echo "AWS version installed:"
     sh -c "aws --version"
-elif [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "2." ]]
-then
+
+elif [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "2." ]]; then
+
     echo "Installing AWS CLI version $AWS_CLI_VERSION"
     curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64-$AWS_CLI_VERSION.zip -o awscliv2.zip \
     && unzip awscliv2.zip \
@@ -22,7 +23,9 @@ then
     /usr/local/aws-cli/v2/*/dist/awscli/examples \
     echo "AWS version installed:"
     sh -c "aws --version"
+
 else
+
     echo "AWS_CLI_VERSION environment variable wasn't set or recognized"
     echo "Installing AWS CLI latest version (2)"
     curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
@@ -36,8 +39,9 @@ else
     /usr/local/aws-cli/v2/*/dist/awscli/examples \
     echo "AWS version installed:"
     sh -c "aws --version"
+
 fi
 
-echo "Executing command: aws $1"
+echo "ACTION EXECUTING COMMAND: aws $1"
 
 sh -c "aws $1"
