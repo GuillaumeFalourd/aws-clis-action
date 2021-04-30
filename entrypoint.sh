3cb59853dafc45e5a10e7aac4f4510cfc5f383a6
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-30 15:29:32 -0300

    update entrypoint file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,32 +2,13 @@
 
 set -e
 
-#if [[ -z $AWS_CLI_VERSION ]]
-#then
-#    echo "AWS_CLI_VERSION environment variable isn't set."
-#    echo "Installing AWS CLI latest version"
-#    pip3 --no-cache-dir install --upgrade awscli
-#else
-#    echo "Installing AWS CLI version AWS_CLI_VERSION"
-#    pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
-#fi
-
-echo "AWS_CLI_VERSION variable set in workflow: $AWS_CLI_VERSION"
-
-if [[ -z $AWS_CLI_VERSION ]]
-then
-    echo "AWS_CLI_VERSION environment variable isn't set."
-    echo "Installing AWS CLI latest version"
-    pip3 --no-cache-dir install --upgrade awscli
-    echo "AWS version installed:"
-    sh -c "aws --version"
-elif [[ ${AWS_CLI_VERSION:0:2} == "1." ]]
+if [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "1." ]]
 then
     echo "Installing AWS CLI version $AWS_CLI_VERSION"
     pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
     echo "AWS version installed:"
     sh -c "aws --version"
-elif [[ ${AWS_CLI_VERSION:0:2} == "2." ]]
+elif [[ ! -z $AWS_CLI_VERSION && ${AWS_CLI_VERSION:0:2} == "2." ]]
 then
     echo "Installing AWS CLI version $AWS_CLI_VERSION"
     curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64-$AWS_CLI_VERSION.zip -o awscliv2.zip \
@@ -42,9 +23,17 @@ then
     echo "AWS version installed:"
     sh -c "aws --version"
 else
-    echo "AWS_CLI_VERSION environment variable wasn't recognized"
-    echo "Installing AWS CLI latest version"
-    pip3 --no-cache-dir install --upgrade awscli
+    echo "AWS_CLI_VERSION environment variable wasn't set or recognized"
+    echo "Installing AWS CLI latest version (2)"
+    curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
+    && unzip awscliv2.zip \
+    && aws/install \
+    && rm -rf \
+    awscliv2.zip \
+    aws \
+    /usr/local/aws-cli/v2/*/dist/aws_completer \
+    /usr/local/aws-cli/v2/*/dist/awscli/data/ac.index \
+    /usr/local/aws-cli/v2/*/dist/awscli/examples \
     echo "AWS version installed:"
     sh -c "aws --version"
 fi
