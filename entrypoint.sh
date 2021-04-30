728f65485eef05e7a283c8e380ce4b5474bb3514
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-04-30 15:07:06 -0300

    update entrypoint file
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,14 +23,14 @@ then
     sh -c "aws --version"
 elif [[ ${AWS_CLI_VERSION:0:2} == "1." ]]
 then
-    echo "Installing AWS CLI version AWS_CLI_VERSION"
+    echo "Installing AWS CLI version $AWS_CLI_VERSION"
     pip3 --no-cache-dir install awscli==$AWS_CLI_VERSION
     echo "AWS version installed:"
     sh -c "aws --version"
 elif [[ ${AWS_CLI_VERSION:0:2} == "2." ]]
 then
-    echo "Installing AWS CLI version AWS_CLI_VERSION"
-    curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip -o awscliv2.zip \
+    echo "Installing AWS CLI version $AWS_CLI_VERSION"
+    curl -sL https://awscli.amazonaws.com/awscli-exe-linux-x86_64-$AWS_CLI_VERSION.zip -o awscliv2.zip \
     && unzip awscliv2.zip \
     && aws/install \
     && rm -rf \
